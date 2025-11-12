6462988133843e9353979bb39f575fb53bbdda48
jerot <jeronimotrujillosadep@gmail.com>
2025-11-11 20:55:07 -0500

    mediocompleto

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert(hola)
+"use strict";
+//Definir los controles
+const txtGrados = document.getElementById("txtGrados");
+const btnConvertir = document.getElementById("btnConvertir");
+const spanResultados = document.getElementById("spanResultados");
+
+btnConvertir.addEventListener("click",()=>{
+  let celsius = Number (txtGrados.value);
+
+  let mensaje = (celsius === "") ? "Ingresa un valor" : `${celsius * 1.8 + 32} °F`;
+  spanResultados.textContent =mensaje;
+  txtGrados.textContent= "";
+}); 
